2e76768a2444acfc32c7156caaa8b36b045b7ed4
yigit pirgayipoglu <ypirgayipoglu24@posta.pau.edu.tr>
2026-07-26 09:18:57 +0300

    Create ProductInfos.cs

---
+++ ProductInfos.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_3
+{
+    internal class ProductInfos
+    {
+        public string productname { get; set; }
+
+        public string price { get; set; }
+
+        public int quantity { get; set; }
+
+
+    }
+}
